462dd16b7e36476b5b13853ffab5b61bf348d556
Perry Lynch <perryklynch@gmail.com>
2022-05-28 21:25:52 -0400

    Added ActionQueue to support undo/redo.

---
+++ StepManiaEditor/ActionQueue.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using Fumen;
+
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Data structure to enqueue EditorActions, and undo and redo those actions
+	/// in the order they were enqueued.
+	/// </summary>
+	public class ActionQueue
+	{
+		/// <summary>
+		/// Index of the current action.
+		/// In other words, the index of the last action which was done.
+		/// If no action was done or we have undone to the beginning, then Index will be -1.
+		/// </summary>
+		private int Index = -1;
+
+		/// <summary>
+		/// The maximum index that can be redone to.
+		/// When undoing actions and then doing new actions, the MaxRedoAction will
+		/// decrease in order to cut off old future actions which can no longer be
+		/// redone.
+		/// </summary>
+		private int MaxRedoIndex = -1;
+
+		/// <summary>
+		/// List of EditorActions for undo and redo.
+		/// This list may be longer than the actual set of EditorActions which can
+		/// be redone in situations where actions are undone and then new actions
+		/// are performed.
+		/// </summary>
+		private readonly List<EditorAction> Actions = new List<EditorAction>();
+
+		/// <summary>
+		/// Do the given EditorAction and add it to the queue of EditorAction for undo and redo.
+		/// </summary>
+		/// <param name="editorAction">EditorAction to do.</param>
+		public void Do(EditorAction editorAction)
+		{
+			// Do the action.
+			Logger.Info(editorAction.ToString());
+			editorAction.Do();
+
+			// Add the action to the list, overwriting any old future action.
+			if (Index == Actions.Count - 1)
+				Actions.Add(editorAction);
+			else
+				Actions[Index + 1] = editorAction;
+			Index++;
+
+			// When an action is added, even if there were future actions now they can
+			// no longer be redone.
+			MaxRedoIndex = Index;
+		}
+
+		/// <summary>
+		/// Clear the queue of EditorActions.
+		/// </summary>
+		public void Clear()
+		{
+			Index = -1;
+			MaxRedoIndex = -1;
+		}
+
+		/// <summary>
+		/// Undo the last action.
+		/// </summary>
+		/// <returns>The action which was undone or null if no action is left to undo.</returns>
+		public EditorAction Undo()
+		{
+			if (Index < 0)
+				return null;
+
+			// Undo the action.
+			Logger.Info($"Undo [{Index + 1}/{MaxRedoIndex + 1}]: {Actions[Index]}");
+			var action = Actions[Index--];
+			action.Undo();
+
+			return action;
+		}
+
+		/// <summary>
+		/// Redo the next action.
+		/// </summary>
+		/// <returns>The action which was redone or null if no action is left to redo.</returns>
+		public EditorAction Redo()
+		{
+			if (Index >= MaxRedoIndex)
+				return null;
+
+			// Redo the action.
+			Logger.Info($"Redo [{Index + 2}/{MaxRedoIndex + 1}]: {Actions[Index + 1]}");
+			var action = Actions[++Index];
+			action.Do();
+
+			return action;
+		}
+	}
+}
---
+++ StepManiaEditor/EditorAction.cs
@@ -0,0 +1,148 @@
+﻿using System.Reflection;
+using Fumen.ChartDefinition;
+
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// An action that can be done and undone.
+	/// Meant to be used by ActionQueue.
+	/// </summary>
+	public abstract class EditorAction
+	{
+		public abstract void Do();
+		public abstract void Undo();
+	}
+
+	/// <summary>
+	/// EditorAction which sets a property on an object to a new value.
+	/// </summary>
+	/// <typeparam name="T">Type of the property.</typeparam>
+	public class ActionSetObjectProperty<T> : EditorAction
+	{
+		private readonly T Value;
+		private readonly T PreviousValue;
+		private readonly object O;
+		private readonly string PropertyName;
+		private readonly PropertyInfo PropertyInfo;
+
+		public ActionSetObjectProperty(object o, string propertyName, T value)
+		{
+			O = o;
+			Value = value;
+			PropertyName = propertyName;
+
+			PropertyInfo = O.GetType().GetProperty(PropertyName, BindingFlags.Public | BindingFlags.Instance);
+			PreviousValue = (T)PropertyInfo.GetValue(O);
+		}
+
+		public ActionSetObjectProperty(object o, string propertyName, T value, T previousValue)
+		{
+			O = o;
+			Value = value;
+			PropertyName = propertyName;
+
+			PropertyInfo = O.GetType().GetProperty(PropertyName, BindingFlags.Public | BindingFlags.Instance);
+			PreviousValue = previousValue;
+		}
+
+		public override string ToString()
+		{
+			return $"Set {O.GetType()} {PropertyName} '{PreviousValue}' > '{Value}'.";
+		}
+
+		public override void Do()
+		{
+			PropertyInfo.SetValue(O, Value, null);
+		}
+
+		public override void Undo()
+		{
+			PropertyInfo.SetValue(O, PreviousValue, null);
+		}
+	}
+
+	/// <summary>
+	/// EditorAction which sets a field on an object to a new value.
+	/// </summary>
+	/// <typeparam name="T">Type of the field.</typeparam>
+	public class ActionSetObjectField<T> : EditorAction
+	{
+		private readonly T Value;
+		private readonly T PreviousValue;
+		private readonly object O;
+		private readonly string FieldName;
+		private readonly FieldInfo FieldInfo;
+
+		public ActionSetObjectField(object o, string fieldName, T value)
+		{
+			O = o;
+			Value = value;
+			FieldName = fieldName;
+
+			FieldInfo = O.GetType().GetField(FieldName, BindingFlags.Public | BindingFlags.Instance);
+			PreviousValue = (T)FieldInfo.GetValue(O);
+		}
+
+		public ActionSetObjectField(object o, string fieldName, T value, T previousValue)
+		{
+			O = o;
+			Value = value;
+			FieldName = fieldName;
+
+			FieldInfo = O.GetType().GetField(FieldName, BindingFlags.Public | BindingFlags.Instance);
+			PreviousValue = previousValue;
+		}
+
+		public override string ToString()
+		{
+			return $"Set {O.GetType()} {FieldInfo} '{PreviousValue}' > '{Value}'.";
+		}
+
+		public override void Do()
+		{
+			FieldInfo.SetValue(O, Value);
+		}
+
+		public override void Undo()
+		{
+			FieldInfo.SetValue(O, PreviousValue);
+		}
+	}
+
+	public class ActionSetExtrasValue<T> : EditorAction
+	{
+		private readonly T Value;
+		private readonly T PreviousValue;
+		private readonly bool PreviousValueSet;
+		private readonly Extras Extras;
+		private readonly string ExtrasKey;
+		private readonly string LogType;
+
+		public ActionSetExtrasValue(string logType, Extras extras, string extrasKey, T value)
+		{
+			LogType = logType;
+			Extras = extras;
+			Value = value;
+			ExtrasKey = extrasKey;
+			PreviousValueSet = Extras.TryGetSourceExtra(ExtrasKey, out PreviousValue);
+		}
+
+		public override string ToString()
+		{
+			return $"Set {LogType} {ExtrasKey} '{PreviousValue}' > '{Value}'.";
+		}
+
+		public override void Do()
+		{
+			Extras.AddSourceExtra(ExtrasKey, Value, true);
+		}
+
+		public override void Undo()
+		{
+			if (PreviousValueSet)
+				Extras.AddSourceExtra(ExtrasKey, PreviousValue, true);
+			else
+				Extras.RemoveSourceExtra(ExtrasKey);
+		}
+	}
+}
