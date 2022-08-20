b30fa42c5fcb295d95c3e046151468f5faa563be
Perry Lynch <perryklynch@gmail.com>
2022-08-20 16:34:39 -0400

    Fixed texture packing bug where it would not utilize free space properly. Extended images when using padding. Added methods for passing in SpriteEffects to allow mirroring.

--- StepManiaEditor/TextureAtlas.cs
+++ StepManiaEditor/TextureAtlas.cs
@@ -45,7 +45,7 @@ namespace StepManiaEditor
 					return null;
 
 				// Texture fits perfectly in this PackNode's bounds.
-				if (twp == Rect.Width || thp == Rect.Height)
+				if (twp == Rect.Width && thp == Rect.Height)
 				{
 					Texture = texture;
 					TextureRect = new Rectangle(Rect.X + padding, Rect.Y + padding, texture.Width, texture.Height);
@@ -129,11 +129,56 @@ namespace StepManiaEditor
 			{
 				var node = kvp.Value;
 				sb.Draw(node.Texture, node.TextureRect, Color.White);
+
+				// If we are using padding, extend the rim of the texture by one to mitigate sampling artifacts.
+				if (Padding > 0)
+				{
+					// Left padding.
+					sb.Draw(node.Texture,
+						new Rectangle(node.TextureRect.X - 1, node.TextureRect.Y, 1, node.TextureRect.Height),
+						new Rectangle(0, 0, 1, node.TextureRect.Height),
+						Color.White);
+
+					// Right padding.
+					sb.Draw(node.Texture,
+						new Rectangle(node.TextureRect.X + node.TextureRect.Width, node.TextureRect.Y, 1, node.TextureRect.Height),
+						new Rectangle(node.TextureRect.Width - 1, 0, 1, node.TextureRect.Height),
+						Color.White);
+
+					// Top padding.
+					sb.Draw(node.Texture,
+						new Rectangle(node.TextureRect.X, node.TextureRect.Y - 1, node.TextureRect.Width, 1),
+						new Rectangle(0, 0, node.TextureRect.Width, 1),
+						Color.White);
+
+					// Bottom padding.
+					sb.Draw(node.Texture,
+						new Rectangle(node.TextureRect.X, node.TextureRect.Y + node.TextureRect.Height, node.TextureRect.Width, 1),
+						new Rectangle(0, node.TextureRect.Height - 1, node.TextureRect.Width, 1),
+						Color.White);
+				}
+
 				node.Texture = null;
 			}
 			sb.End();
 			GraphicsDevice.SetRenderTarget(null);
 			DirtyNodes.Clear();
+		}
+
+		public (int, int) GetDimensions(string textureId)
+		{
+			if (!TextureNodes.TryGetValue(textureId, out var node))
+			{
+				Logger.Warn($"Failed to get dimensions for texture identified by \"{textureId}\". No texture with that id found.");
+				return (0, 0);
+			}
+
+			return (node.TextureRect.Width, node.TextureRect.Height);
+		}
+
+		public void DebugDraw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(Atlas, new Vector2(0, 0), Color.White);
 		}
 
 		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle destinationRectangle, float alpha)
@@ -149,18 +194,59 @@ namespace StepManiaEditor
 
 		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle destinationRectangle, float rotation, float alpha)
 		{
+			Draw(textureId, spriteBatch, destinationRectangle, rotation, alpha, SpriteEffects.None);
+		}
+
+		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle destinationRectangle, float rotation, Vector2 rotationOffset, float alpha, SpriteEffects spriteEffects)
+		{
+			Draw(textureId, spriteBatch, destinationRectangle, rotation, rotationOffset, alpha, SpriteEffects.None);
+		}
+
+		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle destinationRectangle, float rotation, float alpha, SpriteEffects spriteEffects)
+		{
 			if (!TextureNodes.TryGetValue(textureId, out var node))
 			{
 				Logger.Warn($"Failed to draw packed texture identified by \"{textureId}\". No texture with that id found.");
 				return;
 			}
 
-			var rotationOffset = new Vector2(node.TextureRect.Width >> 1, node.TextureRect.Height >> 1);
-			destinationRectangle.X += (destinationRectangle.Width >> 1);
-			destinationRectangle.Y += (destinationRectangle.Height >> 1);
+			// When rotating, we need to provide a center offset to rotate by. For textures with odd lengths, this results
+			// in rounding errors. However we need to go through the SpriteBatch Draw method taking rotations in order to
+			// use SpriteEffects. When using 0.0f rotation, do not apply the offset to mitigate the rounding issues.
+			var rotationOffset = Vector2.Zero;
+			if (rotation != 0.0f)
+			{
+				rotationOffset = new Vector2(node.TextureRect.Width >> 1, node.TextureRect.Height >> 1);
+				destinationRectangle.X += (destinationRectangle.Width >> 1);
+				destinationRectangle.Y += (destinationRectangle.Height >> 1);
+			}
 
-			spriteBatch.Draw(Atlas, destinationRectangle, node.TextureRect, new Color(1.0f, 1.0f, 1.0f, alpha), rotation, rotationOffset,
-				SpriteEffects.None, 1.0f);
+			spriteBatch.Draw(Atlas, destinationRectangle, node.TextureRect, new Color(1.0f, 1.0f, 1.0f, alpha), rotation, rotationOffset, spriteEffects, 1.0f);
+		}
+
+		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle sourceRectangle, Rectangle destinationRectangle, float rotation, float alpha, SpriteEffects spriteEffects)
+		{
+			if (!TextureNodes.TryGetValue(textureId, out var node))
+			{
+				Logger.Warn($"Failed to draw packed texture identified by \"{textureId}\". No texture with that id found.");
+				return;
+			}
+
+			// When rotating, we need to provide a center offset to rotate by. For textures with odd lengths, this results
+			// in rounding errors. However we need to go through the SpriteBatch Draw method taking rotations in order to
+			// use SpriteEffects. When using 0.0f rotation, do not apply the offset to mitigate the rounding issues.
+			var rotationOffset = Vector2.Zero;
+			if (rotation != 0.0f)
+			{
+				rotationOffset = new Vector2(node.TextureRect.Width >> 1, node.TextureRect.Height >> 1);
+				destinationRectangle.X += (destinationRectangle.Width >> 1);
+				destinationRectangle.Y += (destinationRectangle.Height >> 1);
+			}
+
+			sourceRectangle.X += node.TextureRect.X;
+			sourceRectangle.Y += node.TextureRect.Y;
+
+			spriteBatch.Draw(Atlas, destinationRectangle, sourceRectangle, new Color(1.0f, 1.0f, 1.0f, alpha), rotation, rotationOffset, spriteEffects, 1.0f);
 		}
 
 		public void Draw(string textureId, SpriteBatch spriteBatch, Rectangle destinationRectangle, float rotation, Vector2 rotationOffset, float alpha)
@@ -188,21 +274,38 @@ namespace StepManiaEditor
 
 		public void Draw(string textureId, SpriteBatch spriteBatch, Vector2 position, float scale, float rotation, float alpha)
 		{
+			Draw(textureId, spriteBatch, position, scale, rotation, alpha, SpriteEffects.None);
+		}
+
+		public void Draw(string textureId, SpriteBatch spriteBatch, Vector2 position, float scale, float rotation, float alpha, SpriteEffects spriteEffects)
+		{
 			if (!TextureNodes.TryGetValue(textureId, out var node))
 			{
 				Logger.Warn($"Failed to draw packed texture identified by \"{textureId}\". No texture with that id found.");
 				return;
 			}
 
-			var rotationOffset = new Vector2(node.TextureRect.Width >> 1, node.TextureRect.Height >> 1);
-			position.X += node.TextureRect.Width * scale * 0.5f;
-			position.Y += node.TextureRect.Height * scale * 0.5f;
+			// When rotating, we need to provide a center offset to rotate by. For textures with odd lengths, this results
+			// in rounding errors. However we need to go through the SpriteBatch Draw method taking rotations in order to
+			// use SpriteEffects. When using 0.0f rotation, do not apply the offset to mitigate the rounding issues.
+			var rotationOffset = Vector2.Zero;
+			if (rotation != 0.0f)
+			{
+				rotationOffset = new Vector2(node.TextureRect.Width >> 1, node.TextureRect.Height >> 1);
+				position.X += node.TextureRect.Width * scale * 0.5f;
+				position.Y += node.TextureRect.Height * scale * 0.5f;
+			}
 
 			spriteBatch.Draw(Atlas, position, node.TextureRect, new Color(1.0f, 1.0f, 1.0f, alpha), rotation, rotationOffset, scale,
-				SpriteEffects.None, 1.0f);
+				spriteEffects, 1.0f);
 		}
 
 		public void Draw(string textureId, SpriteBatch spriteBatch, Vector2 position, float scale, float rotation, Vector2 rotationOffset, float alpha)
+		{
+			Draw(textureId, spriteBatch, position, scale, rotation, rotationOffset, alpha, SpriteEffects.None);
+		}
+
+		public void Draw(string textureId, SpriteBatch spriteBatch, Vector2 position, float scale, float rotation, Vector2 rotationOffset, float alpha, SpriteEffects spriteEffects)
 		{
 			if (!TextureNodes.TryGetValue(textureId, out var node))
 			{
@@ -211,7 +314,7 @@ namespace StepManiaEditor
 			}
 
 			spriteBatch.Draw(Atlas, position, node.TextureRect, new Color(1.0f, 1.0f, 1.0f, alpha), rotation, rotationOffset, scale,
-				SpriteEffects.None, 1.0f);
+				spriteEffects, 1.0f);
 		}
 	}
 }
