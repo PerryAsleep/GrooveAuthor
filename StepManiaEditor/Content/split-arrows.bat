55ad2e51d9a9433c118e06b585a847f844f8ac2b
Perry Lynch <perryklynch@gmail.com>
2022-10-17 20:45:28 -0400

    Solo diagonal hold/roll start images fixed. They were the incorrect width.

--- StepManiaEditor/Content/split-arrows.bat
+++ StepManiaEditor/Content/split-arrows.bat
@@ -1,3 +1,3 @@
 @REM Tools. Hard-coded and not included in repo. Brittle.
-set CSI=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csi.exe
+set CSI=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\Roslyn\csi.exe
 "%CSI%" split-arrows.csx
