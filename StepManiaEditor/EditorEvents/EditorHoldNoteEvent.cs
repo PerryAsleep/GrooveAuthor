PerryAsleep/GrooveAuthor

commit a50acd82fb1af818e28a38489f30cb2ae77d8525

Editor represents holds with one event instead of two. This removes a lot of special case handling that was starting to surface around repositioning notes and selecting notes. This should also simplify note counts. To support this EditorEvents now support more than one underlying Event. EditorHoldNoteEvent has two Events, the LaneHoldStartNote and the LaneHoldEndNote. EditorEvents are now assumed to possibly have separate start and end values for row, chart time, and chart position. This reduces the need to to perform hold-specific logic outside of EditorEvent. Added Clone method to EditorEvent to support upcoming copy/paste of EditorEvents. Work towards this change motivated the hold refactor. Moved some autoplay logic into new AutoPlayer class. Fixed regression from 99156db94e2548e884ec106c79472c97215a5f0b where finding notes at row 0 for selection would not work. Improved rendering of holds in negative regions where they end before they start. The caps no longer render, matching StepMania. Split more EditorActions into their own files.

StepManiaEditor/EditorEvents/EditorHoldNoteEvent.cs
@@ -1,77 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
 using Fumen.ChartDefinition;
+using Fumen.Converters;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using static System.Diagnostics.Debug;
 using static StepManiaEditor.Utils;
 
 namespace StepManiaEditor
 {
-	internal sealed class EditorHoldEndNoteEvent : EditorEvent
+	internal sealed class EditorHoldNoteEvent : EditorEvent
 	{
-		private EditorHoldStartNoteEvent EditorHoldStartNoteEvent;
+		/// <summary>
+		/// The first underlying Event of the hold: the LaneHoldStartNote.
+		/// </summary>
+		private readonly LaneHoldStartNote LaneHoldStartNote;
+		/// <summary>
+		/// The second underlying Event of the hold: the LaneHoldEndNote.
+		/// </summary>
 		private readonly LaneHoldEndNote LaneHoldEndNote;
+		/// <summary>
+		/// Whether or not this hold is a roll.
+		/// </summary>
+		private bool Roll;
 
 		/// <summary>
-		/// Whether or not this hold should be considered active for rendering.
+		/// Whether or not this hold should be considered active (lit by input/autoplay) for rendering.
 		/// </summary>
 		private bool NextDrawActive;
+		/// <summary>
+		/// When the hold is active the start needs to be brought down to match the receptor.
+		/// This cutoff value is used to cut off the top of the hold and bring the start down.
+		/// </summary>
 		private double NextDrawActiveYCutoffPoint;
 
-		public EditorHoldEndNoteEvent(EventConfig config, LaneHoldEndNote chartEvent) : base(config)
+		public EditorHoldNoteEvent(EventConfig config, LaneHoldStartNote startEvent, LaneHoldEndNote endEvent) : base(config)
 		{
-			LaneHoldEndNote = chartEvent;
+			LaneHoldStartNote = startEvent;
+			LaneHoldEndNote = endEvent;
+			Roll = LaneHoldStartNote.SourceType == SMCommon.NoteChars[(int)SMCommon.NoteType.RollStart].ToString();
 		}
 
-		public void SetHoldStartNote(EditorHoldStartNoteEvent editorHoldStartNoteEvent)
+		/// <summary>
+		/// Static method to create a hold.
+		/// </summary>
+		public static EditorHoldNoteEvent CreateHold(EditorChart chart, int lane, int row, int length, bool roll)
 		{
-			EditorHoldStartNoteEvent = editorHoldStartNoteEvent;
-		}
+			var holdStartTime = 0.0;
+			chart.TryGetTimeFromChartPosition(row, ref holdStartTime);
+			var holdStartNote = new LaneHoldStartNote()
+			{
+				Lane = lane,
+				IntegerPosition = row,
+				TimeSeconds = holdStartTime
+			};
+			var holdEndTime = 0.0;
+			chart.TryGetTimeFromChartPosition(row + length, ref holdEndTime);
+			var holdEndNote = new LaneHoldEndNote()
+			{
+				Lane = lane,
+				IntegerPosition = row + length,
+				TimeSeconds = holdEndTime
+			};
 
-		public EditorHoldStartNoteEvent GetHoldStartNote()
-		{
-			return EditorHoldStartNoteEvent;
+			var config = new EventConfig
+			{
+				EditorChart = chart,
+				ChartEvents = new List<Event> { holdStartNote, holdEndNote },
+			};
+			var hold = new EditorHoldNoteEvent(config, holdStartNote, holdEndNote);
+			hold.SetIsRoll(roll);
+			return hold;
 		}
 
 		public override int GetLane()
 		{
-			return LaneHoldEndNote.Lane;
+			return LaneHoldStartNote.Lane;
 		}
 
 		public override void SetLane(int lane)
 		{
-			if (lane == GetLane())
-				return;
-			base.SetLane(lane);
-
-			// Keep the hold start and end in sync.
-			EditorHoldStartNoteEvent.SetLane(lane);
+			Assert(lane >= 0 && lane < EditorChart.NumInputs);
+			LaneHoldStartNote.Lane = lane;
+			LaneHoldEndNote.Lane = lane; 
 		}
 
 		public override void SetNewPosition(int row)
 		{
-			var len = EditorHoldStartNoteEvent.GetLength();
-			base.SetNewPosition(row);
-
-			if (len != EditorHoldStartNoteEvent.GetLength())
-				EditorHoldStartNoteEvent.UpdatePositionToMaintainLength(len);
+			var len = GetLength();
+			ChartPosition = row;
+			SetNewPositionForEvent(LaneHoldStartNote, row);
+			SetNewPositionForEvent(LaneHoldEndNote, row + len);
 		}
 
-		public void UpdatePositionToMaintainLength(int length)
+		public void RefreshHoldEndTime()
 		{
-			var row = EditorHoldStartNoteEvent.GetRow() + length;
-			base.SetNewPosition(row);
+			SetNewPositionForEvent(LaneHoldEndNote, GetRow() + GetLength());
+		}
+
+		public override int GetLength()
+		{
+			return LaneHoldEndNote.IntegerPosition - LaneHoldStartNote.IntegerPosition;
+		}
+
+		public void SetLength(int length)
+		{
+			SetNewPositionForEvent(LaneHoldEndNote, GetRow() + length);
+		}
+
+		public override double GetEndChartPosition()
+		{
+			return LaneHoldEndNote.IntegerPosition;
+		}
+
+		public override int GetEndRow()
+		{
+			return LaneHoldEndNote.IntegerPosition;
+		}
+
+		public override double GetEndChartTime()
+		{
+			return LaneHoldEndNote.TimeSeconds;
+		}
+
+		public override List<Event> GetEvents()
+		{
+			return new List<Event>() { LaneHoldStartNote, LaneHoldEndNote };
 		}
 
 		public bool IsRoll()
 		{
-			return EditorHoldStartNoteEvent.IsRoll();
+			return Roll;
 		}
 
 		public void SetIsRoll(bool roll)
 		{
-			EditorHoldStartNoteEvent.SetIsRoll(roll);
+			Roll = roll;
+			LaneHoldStartNote.SourceType = Roll ? SMCommon.NoteChars[(int)SMCommon.NoteType.RollStart].ToString() : string.Empty;
 		}
+
+		public override bool IsMiscEvent() { return false; }
+		public override bool IsSelectableWithoutModifiers() { return true; }
+		public override bool IsSelectableWithModifiers() { return false; }
 
 		public void SetNextDrawActive(bool active, double y)
 		{
@@ -79,26 +149,8 @@ namespace StepManiaEditor
 			NextDrawActiveYCutoffPoint = y;
 		}
 
-		public override bool DoesPointIntersect(double x, double y)
-		{
-			// Hold intersections involve the start and end. Checking either
-			// should return the same thing. Leverage the hold start logic.
-			return EditorHoldStartNoteEvent.DoesPointIntersect(x, y);
-		}
-
-		public override List<EditorEvent> GetEventsSelectedTogether()
-		{
-			// Always select both the start and end together.
-			return new List<EditorEvent>() { EditorHoldStartNoteEvent, this };
-		}
-
-		public override bool IsMiscEvent() { return false; }
-		public override bool IsSelectableWithoutModifiers() { return false; }
-		public override bool IsSelectableWithModifiers() { return false; }
-
 		public override void Draw(TextureAtlas textureAtlas, SpriteBatch spriteBatch, ArrowGraphicManager arrowGraphicManager)
 		{
-			var roll = IsRoll();
 			var alpha = IsBeingEdited() ? ActiveEditEventAlpha : Alpha;
 			if (alpha <= 0.0f)
 			{
@@ -109,9 +161,15 @@ namespace StepManiaEditor
 
 			var active = NextDrawActive && Preferences.Instance.PreferencesReceptors.AutoPlayLightHolds;
 			var activeAndCutoff = NextDrawActive && Preferences.Instance.PreferencesReceptors.AutoPlayHideArrows;
+			var selected = IsSelected();
+
+			var (startArrowTexture, holdRot) = arrowGraphicManager.GetArrowTexture(LaneHoldStartNote.IntegerPosition, GetLane(), selected);
+			var (_, startArrowHeight) = textureAtlas.GetDimensions(startArrowTexture);
+			var halfArrowHeight = startArrowHeight * 0.5 * Scale;
+
+			var roll = IsRoll();
 
 			// The hold body texture is a tiled texture that starts at the end of the hold and ends at the arrow.
-			var selected = IsSelected();
 			var (holdBodyTextureId, holdBodyMirrored) = roll ?
 				arrowGraphicManager.GetRollBodyTexture(LaneHoldEndNote.IntegerPosition, LaneHoldEndNote.Lane, active, selected) :
 				arrowGraphicManager.GetHoldBodyTexture(LaneHoldEndNote.IntegerPosition, LaneHoldEndNote.Lane, active, selected);
@@ -122,26 +180,26 @@ namespace StepManiaEditor
 			// The hold start texture is only used to extend the start of the hold upward into the arrow for certain
 			// arrow graphics which wouldn't otherwise mask the hold start, like solo diagonals.
 			var (holdBodyStartTexture, holdBodyStartMirror) = roll ?
-				arrowGraphicManager.GetRollStartTexture(GetHoldStartNote().GetRow(), GetLane(), NextDrawActive, selected) :
-				arrowGraphicManager.GetHoldStartTexture(GetHoldStartNote().GetRow(), GetLane(), NextDrawActive, selected);
+				arrowGraphicManager.GetRollStartTexture(LaneHoldStartNote.IntegerPosition, GetLane(), NextDrawActive, selected) :
+				arrowGraphicManager.GetHoldStartTexture(LaneHoldStartNote.IntegerPosition, GetLane(), NextDrawActive, selected);
 
 			var (_, capH) = textureAtlas.GetDimensions(holdCapTextureId);
 			var (bodyTexW, bodyTexH) = textureAtlas.GetDimensions(holdBodyTextureId);
 
 			// Determine the Y value and height to use.
 			// If the note is active, we should bring down the top to the cutoff point.
-			var noteY = Y;
-			var noteH = H;
+			var bodyY = Y + halfArrowHeight;
+			var noteH = H - halfArrowHeight;
 			if (activeAndCutoff)
 			{
-				noteH -= (NextDrawActiveYCutoffPoint - noteY);
-				noteY = NextDrawActiveYCutoffPoint;
+				noteH -= (NextDrawActiveYCutoffPoint - bodyY);
+				bodyY = NextDrawActiveYCutoffPoint;
 			}
 
 			capH = (int)(capH * Scale + 0.5);
 			var bodyTileH = (int)(bodyTexH * Scale + 0.5);
-			var y = (int)(noteY + noteH + 0.5) - capH;
-			var minY = (int)(noteY + 0.5);
+			var y = (int)(bodyY + noteH + 0.5) - capH;
+			var minY = (int)(bodyY + 0.5);
 			var x = (int)(X + 0.5);
 			var w = (int)(W + 0.5);
 
@@ -197,19 +255,21 @@ namespace StepManiaEditor
 			}
 
 			// Draw the cap, if it is visible.
+			// Also ensure that the cap is below the start. In negative scroll rate regions it may be
+			// above the start, in which case we do not want to render it.
 			// The cap should be drawn after the body as some caps render on top of the body.
-			if (capY > -capH && capY < ScreenHeight)
+			if (capY > -capH && capY < ScreenHeight && capY >= bodyY)
 				textureAtlas.Draw(holdCapTextureId, spriteBatch, new Rectangle(x, capY, w, capH), holdCapRotation, alpha, SpriteEffects.None);
 
-			// If active, draw the hold start note on top of the receptors.
-			// The actual hold start note will not draw since it is above the receptors.
-			if (activeAndCutoff)
-			{
-				var (startArrowTexture, _) = arrowGraphicManager.GetArrowTexture(GetHoldStartNote().GetRow(), GetLane(), selected);
-				var (_, startArrowHeight) = textureAtlas.GetDimensions(startArrowTexture);
-				var holdStartY = noteY - (startArrowHeight * 0.5 * Scale);
-				GetHoldStartNote().DrawAtY(textureAtlas, spriteBatch, arrowGraphicManager, holdStartY);
-			}
+			// Draw the arrow at the start of the hold.
+			var holdStartY = bodyY - halfArrowHeight;
+			textureAtlas.Draw(
+					startArrowTexture,
+					spriteBatch,
+					new Vector2((float)X, (float)holdStartY),
+					Scale,
+					holdRot,
+					alpha);
 
 			// Reset active flags.
 			NextDrawActive = false;

StepManiaEditor/AutoPlayer.cs
@@ -1,0 +1,172 @@
+﻿using Fumen.ChartDefinition;
+using System.Linq;
+
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Class for managing autoplay input on the active Receptors for a given EditorChart.
+	/// </summary>
+	internal sealed class AutoPlayer
+	{
+		/// <summary>
+		/// Tracked state per lane.
+		/// Used for comparing previous update values to current values to issue autoplay input.
+		/// </summary>
+		private sealed class AutoPlayState
+		{
+			/// <summary>
+			/// The next EditorEvent relevant for input. May be null in the case of releasing holds.
+			/// </summary>
+			public EditorEvent NextEvent { get; private set; }
+			/// <summary>
+			/// The row of the next input event. If there is no next input it is -1.
+			/// </summary>
+			public int NextEventRow { get; private set; }
+			/// <summary>
+			/// The chart time of the next input event. If there is no next input it is 0.0.
+			/// </summary>
+			public double NextEventTime { get; private set; }
+			/// <summary>
+			/// Flag for whether or not this state is set to valid data or not. When it is not
+			/// set to valid data we do not want to use for comparisons as it would result in
+			/// autoplaying the lane incorrectly when playback begins.
+			/// </summary>
+			public bool IsUnset { get; private set; }
+
+			public AutoPlayState()
+			{
+				Reset();
+			}
+
+			public void Reset()
+			{
+				IsUnset = true;
+				NextEventRow = -1;
+				NextEventTime = 0.0;
+				NextEvent = null;
+			}
+
+			public void Update(int nextRow, double nextTime, EditorEvent nextEvent)
+			{
+				IsUnset = false;
+				NextEvent = nextEvent;
+				NextEventRow = nextRow;
+				NextEventTime = nextTime;
+			}
+
+			public bool HasValidNextEvent()
+			{
+				if (IsUnset || (NextEvent == null && NextEventRow < 0))
+					return false;
+				return true;
+			}
+
+			public bool IsNextEventHoldRelease()
+			{
+				return NextEvent == null && NextEventRow > 0;
+			}
+		}
+
+		private AutoPlayState[] AutoPlayStates = null;
+		private Receptor[] Receptors = null;
+		private EditorChart ActiveChart = null;
+
+		public AutoPlayer(EditorChart chart, Receptor[] receptors)
+		{
+			Receptors = receptors;
+			ActiveChart = chart;
+			AutoPlayStates = new AutoPlayState[ActiveChart.NumInputs];
+			for(var i = 0; i < AutoPlayStates.Count(); i++)
+				AutoPlayStates[i] = new AutoPlayState();
+		}
+
+		/// <summary>
+		/// Update autoplay with the given EditorPostion.
+		/// </summary>
+		/// <param name="position">The current position of the chart.</param>
+		public void Update(EditorPosition position)
+		{
+			if (ActiveChart == null || AutoPlayStates == null || Receptors == null)
+				return;
+
+			// Gather all the next input events for the current position.
+			var nextInputs = ActiveChart.GetNextInputs(position.ChartPosition);
+
+			// Check each lane.
+			for (var lane = 0; lane < ActiveChart.NumInputs; lane++)
+			{
+				var nextEventRow = nextInputs[lane].Item1;
+				var nextEvent = nextInputs[lane].Item2;
+				var nextEventTime = 0.0;
+				var nextEventIsHoldEnd = nextEvent == null && nextEventRow > 0;
+				ActiveChart.TryGetTimeFromChartPosition(nextEventRow, ref nextEventTime);
+
+				// If the previous call's next event row doesn't match the current next event row
+				// then something as changed and we should update input.
+				if (AutoPlayStates[lane].NextEventRow != nextEventRow)
+				{
+					// Since we have already passed a note, we should offset any animations so they begin
+					// as if they started at the precise moment the event passed.
+					var timeDelta = AutoPlayStates[lane].IsUnset ? 0.0 :
+						position.ChartTime - AutoPlayStates[lane].NextEventTime;
+
+					// The new next event can be null at the end of the chart. We need to release any
+					// held input in this case.
+					if (nextEventRow < 0 && Receptors[lane].IsAutoplayHeld())
+					{
+						Receptors[lane].OnAutoplayInputUp(timeDelta);
+					}
+
+					// Only process inputs if the last state is valid.
+					// This helps ensure that when starting playing in the middle of a chart
+					// we don't incorrectly show input immediately.
+					if (AutoPlayStates[lane].HasValidNextEvent())
+					{
+						// If the event that just passed is a hold end, release input.
+						if (AutoPlayStates[lane].IsNextEventHoldRelease())
+						{
+							Receptors[lane].OnAutoplayInputUp(timeDelta);
+						}
+						else
+						{
+							// For both taps an hold starts, press input.
+							Receptors[lane].OnAutoplayInputDown(timeDelta);
+
+							// For taps, release them immediately.
+							if (AutoPlayStates[lane].NextEvent.GetFirstEvent() is LaneTapNote)
+							{
+								Receptors[lane].OnAutoplayInputUp(timeDelta);
+							}
+						}
+					}
+
+					// If the next event is a hold end (i.e. we are in a hold) and the currently
+					// state is unset (i.e. we just started playback), then start input to
+					// hold the note.
+					else if (AutoPlayStates[lane].IsUnset && nextEventIsHoldEnd)
+					{
+						Receptors[lane].OnAutoplayInputDown(timeDelta);
+					}
+				}
+
+				// Update the state for next time.
+				AutoPlayStates[lane].Update(nextEventRow, nextEventTime, nextEvent);
+			}
+		}
+
+		/// <summary>
+		/// Stop all autoplay input.
+		/// </summary>
+		public void Stop()
+		{
+			if (ActiveChart == null || AutoPlayStates == null || Receptors == null)
+				return;
+
+			for (var lane = 0; lane < ActiveChart.NumInputs; lane++)
+			{
+				AutoPlayStates[lane].Reset();
+				Receptors[lane].OnAutoplayInputCancel();
+			}
+		}
+	}
+}

StepManiaEditor/EditorActions/ActionAddChart.cs
@@ -1,0 +1,50 @@
+﻿using static Fumen.Converters.SMCommon;
+
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Action to add a chart to the active song.
+	/// </summary>
+	internal sealed class ActionAddChart : EditorAction
+	{
+		private Editor Editor;
+		private ChartType ChartType;
+		private EditorChart AddedChart;
+		private EditorChart PreivouslyActiveChart;
+
+		public ActionAddChart(Editor editor, ChartType chartType)
+		{
+			Editor = editor;
+			ChartType = chartType;
+		}
+
+		public override bool AffectsFile()
+		{
+			return true;
+		}
+
+		public override string ToString()
+		{
+			return $"Add {Utils.GetPrettyEnumString(ChartType)} Chart.";
+		}
+
+		public override void Do()
+		{
+			PreivouslyActiveChart = Editor.GetActiveChart();
+
+			// Through undoing and redoing we may add the same chart multiple times.
+			// Other actions like ActionAddEditorEvent reference specific charts.
+			// For those actions to work as expected we should restore the same chart instance
+			// rather than creating a new one when undoing and redoing.
+			if (AddedChart != null)
+				Editor.AddChart(AddedChart, true);
+			else
+				AddedChart = Editor.AddChart(ChartType, true);
+		}
+
+		public override void Undo()
+		{
+			Editor.DeleteChart(AddedChart, PreivouslyActiveChart);
+		}
+	}
+}

StepManiaEditor/EditorActions/ActionDeleteChart.cs
@@ -1,0 +1,40 @@
+﻿
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Action to delete a chart from the active song.
+	/// </summary>
+	internal sealed class ActionDeleteChart : EditorAction
+	{
+		private Editor Editor;
+		private EditorChart Chart;
+		private bool DeletedActiveChart;
+
+		public ActionDeleteChart(Editor editor, EditorChart chart)
+		{
+			Editor = editor;
+			Chart = chart;
+		}
+
+		public override bool AffectsFile()
+		{
+			return true;
+		}
+
+		public override string ToString()
+		{
+			return $"Delete {Utils.GetPrettyEnumString(Chart.ChartType)} Chart.";
+		}
+
+		public override void Do()
+		{
+			DeletedActiveChart = Editor.GetActiveChart() == Chart;
+			Editor.DeleteChart(Chart, null);
+		}
+
+		public override void Undo()
+		{
+			Editor.AddChart(Chart, DeletedActiveChart);
+		}
+	}
+}

StepManiaEditor/EditorActions/ActionMoveFocalPoint.cs
@@ -1,0 +1,44 @@
+﻿
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Action to move the focal point of the receptors to a new location.
+	/// </summary>
+	internal sealed class ActionMoveFocalPoint : EditorAction
+	{
+		private int PreviousX;
+		private int PreviousY;
+		private int NewX;
+		private int NewY;
+
+		public ActionMoveFocalPoint(int previousX, int previousY, int newX, int newY)
+		{
+			PreviousX = previousX;
+			PreviousY = previousY;
+			NewX = newX;
+			NewY = newY;
+		}
+
+		public override bool AffectsFile()
+		{
+			return false;
+		}
+
+		public override string ToString()
+		{
+			return $"Move receptors from ({PreviousX}, {PreviousY}) to ({NewX}, {NewY}).";
+		}
+
+		public override void Do()
+		{
+			Preferences.Instance.PreferencesReceptors.PositionX = NewX;
+			Preferences.Instance.PreferencesReceptors.PositionY = NewY;
+		}
+
+		public override void Undo()
+		{
+			Preferences.Instance.PreferencesReceptors.PositionX = PreviousX;
+			Preferences.Instance.PreferencesReceptors.PositionY = PreviousY;
+		}
+	}
+}

StepManiaEditor/EditorActions/ActionMultiple.cs
@@ -1,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace StepManiaEditor
+{
+	/// <summary>
+	/// Action to perform multiple other actions together as a single action.
+	/// </summary>
+	internal sealed class ActionMultiple : EditorAction
+	{
+		private readonly List<EditorAction> Actions;
+
+		public ActionMultiple()
+		{
+			Actions = new List<EditorAction>();
+		}
+
+		public ActionMultiple(List<EditorAction> actions)
+		{
+			Actions = actions;
+		}
+
+		public void EnqueueAndDo(EditorAction action)
+		{
+			action.Do();
+			Actions.Add(action);
+		}
+
+		public void EnqueueWithoutDoing(EditorAction action)
+		{
+			Actions.Add(action);
+		}
+
+		public List<EditorAction> GetActions()
+		{
+			return Actions;
+		}
+
+		public override bool AffectsFile()
+		{
+			foreach (var action in Actions)
+			{
+				if (action.AffectsFile())
+					return true;
+			}
+			return false;
+		}
+
+		public override string ToString()
+		{
+			return string.Join(' ', Actions);
+		}
+
+		public override void Do()
+		{
+			foreach (var action in Actions)
+			{
+				action.Do();
+			}
+		}
+
+		public override void Undo()
+		{
+			var i = Actions.Count - 1;
+			while (i >= 0)
+			{
+				Actions[i--].Undo();
+			}
+		}
+
+		public void Clear()
+		{
+			Actions.Clear();
+		}
+	}
+}

StepManiaEditor/EditorActions/ActionPasteCopiedEvents.cs
@@ -1,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepManiaEditor.EditorActions
+{
+	internal class ActionPasteCopiedEvents
+	{
+	}
+}

commit 905ffa956b262dd8c7a38ee2020a90467f788b8a

Fix Pump hold ends not rendering on short holds. Regression from a50acd82fb1af818e28a38489f30cb2ae77d8525.

StepManiaEditor/EditorEvents/EditorHoldNoteEvent.cs
@@ -201,8 +201,12 @@ namespace StepManiaEditor
 
 			// Record the cap position for drawing later.
 			var capY = y;
+			var minimumCapY = bodyY;
 			if (arrowGraphicManager.AreHoldCapsCentered())
+			{
 				y += (int)(capH * 0.5f);
+				minimumCapY = Y;
+			}
 
 			// Adjust the starting y value so we don't needlessly loop when zoomed in and a large
 			// area of the hold is off the screen.
@@ -254,7 +258,7 @@ namespace StepManiaEditor
 			// Also ensure that the cap is below the start. In negative scroll rate regions it may be
 			// above the start, in which case we do not want to render it.
 			// The cap should be drawn after the body as some caps render on top of the body.
-			if (capY > -capH && capY < ScreenHeight && capY >= bodyY)
+			if (capY > -capH && capY < ScreenHeight && capY >= minimumCapY)
 				textureAtlas.Draw(holdCapTextureId, spriteBatch, new Rectangle(x, capY, w, capH), holdCapRotation, alpha, SpriteEffects.None);
 
 			// Draw the arrow at the start of the hold.