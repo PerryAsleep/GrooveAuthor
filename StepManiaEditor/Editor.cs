PerryAsleep/GrooveAuthor

commit bb0ead48f9da3e4ba7e751f20227808226c62641

Fix flickering cursor when moving over receptors.

StepManiaEditor/Editor.cs
@@ -294,8 +294,8 @@ internal sealed class Editor :
 	private SpriteFont Font;
 
 	// Cursor
-	private Cursor CurrentDesiredCursor = Cursors.Default;
-	private Cursor PreviousDesiredCursor = Cursors.Default;
+	private MouseCursor CurrentDesiredCursor = MouseCursor.Arrow;
+	private MouseCursor PreviousDesiredCursor = MouseCursor.Arrow;
 
 	// Performance Monitoring
 	private PerformanceMonitor PerformanceMonitor;
@@ -1512,7 +1512,7 @@ internal sealed class Editor :
 	{
 		var inFocus = IsApplicationFocused();
 
-		CurrentDesiredCursor = Cursors.Default;
+		CurrentDesiredCursor = MouseCursor.Arrow;
 		CanShowRightClickPopupThisFrame = false;
 
 		SelectedRegion.UpdateTime(currentTime);
@@ -1572,7 +1572,7 @@ internal sealed class Editor :
 			ProcessInputForMovingFocalPoint(inReceptorArea);
 			// Update cursor based on whether the receptors could be grabbed.
 			if (inReceptorArea && !Preferences.Instance.PreferencesReceptors.LockPosition)
-				CurrentDesiredCursor = Cursors.SizeAll;
+				CurrentDesiredCursor = MouseCursor.SizeAll;
 		}
 
 		// Process input for selecting a region.
@@ -1586,9 +1586,9 @@ internal sealed class Editor :
 		// behavior like indicating resizeability at the edges of the window. But not setting every frame
 		// causes it to go back to the Default. Set it every frame only if it setting it to something
 		// other than the Default.
-		if (CurrentDesiredCursor != PreviousDesiredCursor || CurrentDesiredCursor != Cursors.Default)
+		if (CurrentDesiredCursor != PreviousDesiredCursor || CurrentDesiredCursor != MouseCursor.Arrow)
 		{
-			Cursor.Current = CurrentDesiredCursor;
+			Mouse.SetCursor(CurrentDesiredCursor);
 		}
 
 		PreviousDesiredCursor = CurrentDesiredCursor;

commit b56235ed8d636cc74a37d5d6c579101af60910d9

Fix arrow cursor getting stuck when moving from receptors to gui windows. Regression from bb0ead4.

StepManiaEditor/Editor.cs
@@ -1551,6 +1551,7 @@ internal sealed class Editor :
 			// ImGui may want the mouse on a release when we are selecting. Stop selecting in that case.
 			if (SelectedRegion.IsActive())
 				FinishSelectedRegion();
+			UpdateCursor();
 			return;
 		}
 
@@ -1583,6 +1584,14 @@ internal sealed class Editor :
 		// Process right click popup eligibility.
 		CanShowRightClickPopupThisFrame = !MiniMapCapturingMouse && !MovingFocalPoint && !MovingFocalPoint;
 
+		UpdateCursor();
+
+		// Process input for scrolling and zooming.
+		ProcessInputForScrollingAndZooming(currentTime, gameTime.ElapsedGameTime.TotalSeconds);
+	}
+
+	private void UpdateCursor()
+	{
 		// Setting the cursor every frame prevents it from changing to support normal application
 		// behavior like indicating resizeability at the edges of the window. But not setting every frame
 		// causes it to go back to the Default. Set it every frame only if it setting it to something
@@ -1593,9 +1602,6 @@ internal sealed class Editor :
 		}
 
 		PreviousDesiredCursor = CurrentDesiredCursor;
-
-		// Process input for scrolling and zooming.
-		ProcessInputForScrollingAndZooming(currentTime, gameTime.ElapsedGameTime.TotalSeconds);
 	}
 
 	/// <summary>