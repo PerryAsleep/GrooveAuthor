4e393d787fbbd4513ef2d33bb0e59e3a3d460794
Perry Lynch <perryklynch@gmail.com>
2024-05-27 20:23:41 -0400

    Add MonoGame Color interpolation functions.

--- MonoGameExtensions/ColorUtils.cs
+++ MonoGameExtensions/ColorUtils.cs
@@ -8,4 +8,14 @@ public class ColorUtils
 	{
 		return Fumen.ColorUtils.ToBGR565((float)c.R / byte.MaxValue, (float)c.G / byte.MaxValue, (float)c.B / byte.MaxValue);
 	}
+
+	public static Color Interpolate(uint startColor, uint endColor, float percent)
+	{
+		return new Color(Fumen.ColorUtils.ColorRGBAInterpolate(startColor, endColor, percent));
+	}
+
+	public static Color Interpolate(Color startColor, Color endColor, float percent)
+	{
+		return new Color(Fumen.ColorUtils.ColorRGBAInterpolate(startColor.PackedValue, endColor.PackedValue, percent));
+	}
 }
