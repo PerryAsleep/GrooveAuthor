PerryAsleep/GrooveAuthor

commit 05c2b5c7386240eafae2a4e877a42038dafe1d1b

#13: Fix fake segments to use lengths in rows instead of time.

StepManiaEditor/EditorEvents/EditorFakeSegmentEvent.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGameExtensions;
 using static System.Diagnostics.Debug;
-using static Fumen.FumenExtensions;
 using static StepManiaEditor.Utils;
 
 namespace StepManiaEditor;
@@ -17,17 +16,16 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 	public static readonly string WidgetHelp =
 		"Fake Region.\n" +
 		EventShortDescription + "\n" +
-		"Expected format: \"<time>s\". e.g. \"1.0s\"\n" +
-		"Fake region lengths are in seconds and must be non-negative.";
+		"Expected format: \"<length>rows\". e.g. \"48rows\"\n" +
+		"Length must be non-negative.";
 
-	public const double MinFakeSegmentLength = 0.000001;
+	public const int MinFakeSegmentLength = 1;
 
-	private const string Format = "%.9gs";
-	private const float Speed = 0.01f;
+	private const string Format = "%irows";
+	private const float Speed = 1.0f;
 
 	private readonly FakeSegment FakeSegmentEvent;
 	private bool WidthDirty;
-	private double EndChartPosition;
 
 	#region IChartRegion Implementation
 
@@ -95,28 +93,28 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 
 	#endregion IChartRegion Implementation
 
-	public double DoubleValue
+	public int IntValue
 	{
-		get => FakeSegmentEvent.LengthSeconds;
+		get => FakeSegmentEvent.LengthIntegerPosition;
 		set
 		{
 			Assert(EditorChart.CanBeEdited());
 			if (!EditorChart.CanBeEdited())
 				return;
 
-			if (value >= MinFakeSegmentLength && !FakeSegmentEvent.LengthSeconds.DoubleEquals(value))
+			if (FakeSegmentEvent.LengthIntegerPosition != value && value >= MinFakeSegmentLength)
 			{
-				var oldEndTime = GetEndChartTime();
-				FakeSegmentEvent.LengthSeconds = value;
+				var oldPosition = GetEndChartPosition();
+				FakeSegmentEvent.LengthIntegerPosition = value;
 				WidthDirty = true;
-				EditorChart.OnFakeSegmentTimeModified(this, oldEndTime, GetEndChartTime());
+				EditorChart.OnFakeSegmentLengthModified(this, oldPosition, GetEndChartPosition());
 			}
 		}
 	}
 
-	public double GetFakeTimeSeconds()
+	public int GetFakeLengthRows()
 	{
-		return FakeSegmentEvent.LengthSeconds;
+		return FakeSegmentEvent.LengthIntegerPosition;
 	}
 
 	/// <remarks>
@@ -134,7 +132,7 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 		{
 			if (WidthDirty)
 			{
-				WidthInternal = ImGuiLayoutUtils.GetMiscEditorEventDragDoubleWidgetWidth(DoubleValue, Format);
+				WidthInternal = ImGuiLayoutUtils.GetMiscEditorEventDragIntWidgetWidth(IntValue, Format);
 				WidthDirty = false;
 			}
 
@@ -148,9 +146,9 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 		FakeSegmentEvent = chartEvent;
 		WidthDirty = true;
 
-		Assert(FakeSegmentEvent.LengthSeconds >= MinFakeSegmentLength);
-		if (FakeSegmentEvent.LengthSeconds < MinFakeSegmentLength)
-			FakeSegmentEvent.LengthSeconds = MinFakeSegmentLength;
+		Assert(FakeSegmentEvent.LengthIntegerPosition >= MinFakeSegmentLength);
+		if (FakeSegmentEvent.LengthIntegerPosition < MinFakeSegmentLength)
+			FakeSegmentEvent.LengthIntegerPosition = MinFakeSegmentLength;
 	}
 
 	public override string GetShortTypeName()
@@ -173,34 +171,24 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 		return true;
 	}
 
-	public void RefreshEndChartPosition()
+	public override double GetEndChartPosition()
 	{
-		EditorChart.TryGetChartPositionFromTime(GetEndChartTime(), ref EndChartPosition);
+		return GetChartPosition() + FakeSegmentEvent.LengthIntegerPosition;
 	}
 
 	public override int GetEndRow()
 	{
-		return (int)EndChartPosition;
-	}
-
-	public override double GetEndChartPosition()
-	{
-		return EndChartPosition;
-	}
-
-	public override double GetEndChartTime()
-	{
-		return GetChartTime() + FakeSegmentEvent.LengthSeconds;
+		return GetRow() + FakeSegmentEvent.LengthIntegerPosition;
 	}
 
 	public override void Draw(TextureAtlas textureAtlas, SpriteBatch spriteBatch, ArrowGraphicManager arrowGraphicManager)
 	{
 		if (Alpha <= 0.0f)
 			return;
-		ImGuiLayoutUtils.MiscEditorEventDragDoubleWidget(
+		ImGuiLayoutUtils.MiscEditorEventDragIntWidget(
 			GetImGuiId(),
 			this,
-			nameof(DoubleValue),
+			nameof(IntValue),
 			(int)X, (int)Y, (int)W,
 			UIFakesColorRGBA,
 			IsSelected(),

StepManiaEditor/EditorChart.cs
@@ -690,11 +690,11 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 				}
 				case FakeSegment f:
 				{
-					if (f.LengthSeconds < EditorFakeSegmentEvent.MinFakeSegmentLength)
+					if (f.LengthIntegerPosition < EditorFakeSegmentEvent.MinFakeSegmentLength)
 					{
 						LogWarn(
-							$"Fake Segment {f.LengthSeconds} at row {f.IntegerPosition} is below the minimum length of {EditorFakeSegmentEvent.MinFakeSegmentLength} seconds. Clamping to {EditorFakeSegmentEvent.MinFakeSegmentLength}.");
-						f.LengthSeconds = EditorFakeSegmentEvent.MinFakeSegmentLength;
+							$"Fake Segment {f.LengthIntegerPosition} at row {f.IntegerPosition} is below the minimum length of {EditorFakeSegmentEvent.MinFakeSegmentLength} rows. Clamping to {EditorFakeSegmentEvent.MinFakeSegmentLength}.");
+						f.LengthIntegerPosition = EditorFakeSegmentEvent.MinFakeSegmentLength;
 					}
 
 					editorEvent = EditorEvent.CreateEvent(EventConfig.CreateConfig(this, chartEvent, false));
@@ -1165,7 +1165,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 			switch (editorEvent)
 			{
 				case EditorFakeSegmentEvent fse:
-					fakes.Insert(fse, fse.GetChartTime(), fse.GetEndChartTime());
+					fakes.Insert(fse, fse.GetChartPosition(), fse.GetEndChartPosition());
 					break;
 				case EditorStopEvent se:
 					stops.Insert(se, se.GetChartTime(), Math.Max(se.GetChartTime(), se.GetEndChartTime()));
@@ -1236,7 +1236,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		var rowsPerBeat = MaxValidDenominator;
 		var beatsPerMeasure = NumBeatsPerMeasure;
 		var totalStopTimeSeconds = 0.0;
-		var currentFakeTimeEndSeconds = 0.0;
+		var currentFakeEndRow = -1;
 		var previousEventTimeSeconds = 0.0;
 
 		var currentRow = -1;
@@ -1327,7 +1327,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 				}
 			}
 
-			var fakeDueToRow = warpingEndPosition != -1 || warpedOverDueToNegativeStop || currentFakeTimeEndSeconds > eventTime;
+			var fakeDueToRow = warpingEndPosition != -1 || warpedOverDueToNegativeStop || currentFakeEndRow > row;
 			editorEvent.SetRowDependencies(eventTime, rowRelativeToMeasureStart, (short)lastTimeSigDenominator, fakeDueToRow);
 			previousEventTimeSeconds = eventTime;
 
@@ -1381,8 +1381,8 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 				}
 				case EditorFakeSegmentEvent fakeSegment:
 				{
-					currentFakeTimeEndSeconds = eventTime + fakeSegment.GetFakeTimeSeconds();
-					fakes.Insert(fakeSegment, fakeSegment.GetChartTime(), fakeSegment.GetEndChartTime());
+					currentFakeEndRow = row + fakeSegment.GetFakeLengthRows();
+					fakes.Insert(fakeSegment, fakeSegment.GetChartPosition(), fakeSegment.GetEndChartPosition());
 					break;
 				}
 				// Warp handling. Update warp start and stop rows so we can compute the warp time.
@@ -1492,8 +1492,6 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 			((EditorHoldNoteEvent)hold).RefreshHoldEndTime();
 		foreach (var stop in stops)
 			stop.RefreshEndChartPosition();
-		foreach (var fakeSegment in fakes)
-			fakeSegment.RefreshEndChartPosition();
 
 		EditorEvents.Validate();
 
@@ -1877,7 +1875,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		var delays = GetDelayEventOverlapping(chartTime);
 		if (delays != null)
 			regionEvents.AddRange(delays);
-		var fakes = GetFakeSegmentEventOverlapping(chartTime);
+		var fakes = GetFakeSegmentEventOverlapping(chartPosition);
 		if (fakes != null)
 			regionEvents.AddRange(fakes);
 		var warps = GetWarpEventOverlapping(chartPosition);
@@ -1905,9 +1903,9 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		return Delays?.FindAllOverlapping(chartTime);
 	}
 
-	private List<EditorFakeSegmentEvent> GetFakeSegmentEventOverlapping(double chartTime)
+	private List<EditorFakeSegmentEvent> GetFakeSegmentEventOverlapping(double chartPosition)
 	{
-		return Fakes?.FindAllOverlapping(chartTime);
+		return Fakes?.FindAllOverlapping(chartPosition);
 	}
 
 	private List<EditorWarpEvent> GetWarpEventOverlapping(double chartPosition)
@@ -2042,15 +2040,15 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		var time = editorEvent.GetChartTime();
 
 		// An event in a fake region is a fake.
-		if (Fakes.FindAllOverlapping(time, true, false).Count > 0)
+		if (Fakes.FindAllOverlapping(chartPosition, true, false).Count > 0)
 			return true;
 
 		// An event in a warp region is a fake.
-		var overlappingWarps = Warps.FindAllOverlapping(editorEvent.GetChartPosition(), true, false);
+		var overlappingWarps = Warps.FindAllOverlapping(chartPosition, true, false);
 		foreach (var warp in overlappingWarps)
 		{
 			// Warps which are coincident with stops or delays do not cause simultaneous notes to be warped over.
-			if (warp.GetRow() == editorEvent.GetRow())
+			if (warp.GetRow() == row)
 			{
 				var simultaneousStop = RateAlteringEvents.FindEventAtRow<EditorStopEvent>(row);
 				if (simultaneousStop != null)
@@ -2194,18 +2192,18 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 	}
 
 	/// <summary>
-	/// Called when an EditorFakeSegmentEvent's time is modified.
+	/// Called when an EditorFakeSegmentEvent's length is modified.
 	/// </summary>
-	public void OnFakeSegmentTimeModified(EditorFakeSegmentEvent fake, double oldEndTime, double newEndTime)
+	public void OnFakeSegmentLengthModified(EditorFakeSegmentEvent fake, double oldEndPosition, double newEndPosition)
 	{
 		Assert(CanBeEdited());
 		if (!CanBeEdited())
 			return;
 
 		// Update the IntervalTree holding this Fake.
-		var deleted = Fakes.Delete(fake, fake.GetChartTime(), oldEndTime);
+		var deleted = Fakes.Delete(fake, fake.GetChartPosition(), oldEndPosition);
 		Assert(deleted);
-		Fakes.Insert(fake, fake.GetChartTime(), newEndTime);
+		Fakes.Insert(fake, fake.GetChartPosition(), newEndPosition);
 
 		// Fake segments affect row-dependent fake data.
 		RefreshEventTimingData();
@@ -2391,7 +2389,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		switch (editorEvent)
 		{
 			case EditorFakeSegmentEvent fse:
-				deleted = Fakes.Delete(fse, fse.GetChartTime(), fse.GetEndChartTime());
+				deleted = Fakes.Delete(fse, fse.GetChartPosition(), fse.GetEndChartPosition());
 				Assert(deleted);
 				rowDependentDataDirty = true;
 				break;
@@ -2525,7 +2523,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		switch (editorEvent)
 		{
 			case EditorFakeSegmentEvent fse:
-				Fakes.Insert(fse, fse.GetChartTime(), fse.GetEndChartTime());
+				Fakes.Insert(fse, fse.GetChartPosition(), fse.GetEndChartPosition());
 				rowDependentDataDirty = true;
 				break;
 			case EditorRateAlteringEvent rae:

StepManiaEditor/EditorEvents/EventConfig.cs
@@ -449,11 +449,11 @@ internal sealed class EventConfig
 	/// </summary>
 	/// <param name="chart">EditorChart to own the new fake segment.</param>
 	/// <param name="row">Fake segment row.</param>
-	/// <param name="fakeTime">Fake segment length in seconds.</param>
+	/// <param name="fakeLength">Fake segment length in rows.</param>
 	/// <returns>EventConfig for the new EditorFakeSegmentEvent.</returns>
-	public static EventConfig CreateFakeConfig(EditorChart chart, int row, double fakeTime)
+	public static EventConfig CreateFakeConfig(EditorChart chart, int row, int fakeLength = MaxValidDenominator)
 	{
-		return new EventConfig(chart, new FakeSegment(fakeTime)
+		return new EventConfig(chart, new FakeSegment(fakeLength)
 		{
 			IntegerPosition = row,
 		});

StepManiaEditor/StepTotals.cs
@@ -106,17 +106,27 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 
 	public void OnEventAdded(EditorEvent editorEvent)
 	{
+		OnEventAdded(editorEvent, editorEvent.IsFake());
+	}
+
+	private static bool CanEventBeConsideredFake(EditorEvent editorEvent)
+	{
+		return editorEvent.IsLaneNote();
+	}
+
+	private void OnEventAdded(EditorEvent editorEvent, bool isFake)
+	{
 		var isStep = false;
 		switch (editorEvent)
 		{
 			case EditorTapNoteEvent:
-				if (editorEvent.IsFake())
+				if (isFake)
 					FakeCount++;
 				else
 					isStep = true;
 				break;
 			case EditorHoldNoteEvent hold:
-				if (editorEvent.IsFake())
+				if (isFake)
 				{
 					FakeCount++;
 				}
@@ -137,7 +147,7 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 				FakeCount++;
 				break;
 			case EditorLiftNoteEvent:
-				if (editorEvent.IsFake())
+				if (isFake)
 				{
 					FakeCount++;
 				}
@@ -171,17 +181,22 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 
 	public void OnEventDeleted(EditorEvent editorEvent)
 	{
+		OnEventDeleted(editorEvent, editorEvent.IsFake());
+	}
+
+	private void OnEventDeleted(EditorEvent editorEvent, bool isFake)
+	{
 		var isStep = false;
 		switch (editorEvent)
 		{
 			case EditorTapNoteEvent:
-				if (editorEvent.IsFake())
+				if (isFake)
 					FakeCount--;
 				else
 					isStep = true;
 				break;
 			case EditorHoldNoteEvent hold:
-				if (editorEvent.IsFake())
+				if (isFake)
 				{
 					FakeCount--;
 				}
@@ -202,7 +217,7 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 				FakeCount--;
 				break;
 			case EditorLiftNoteEvent:
-				if (editorEvent.IsFake())
+				if (isFake)
 				{
 					FakeCount--;
 				}
@@ -251,15 +266,17 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 
 	public void OnFakeTypeChanged(EditorEvent editorEvent)
 	{
+		if (!CanEventBeConsideredFake(editorEvent))
+			return;
 		if (!editorEvent.IsFake())
 		{
 			FakeCount--;
-			OnEventAdded(editorEvent);
+			OnEventAdded(editorEvent, false);
 		}
 		else
 		{
 			FakeCount++;
-			OnEventDeleted(editorEvent);
+			OnEventDeleted(editorEvent, false);
 		}
 	}
 

StepManiaEditor/UI/UIEditEvents.cs
@@ -474,11 +474,8 @@ internal sealed class UIEditEvents
 
 			ImGui.Separator();
 			DrawAddEventMenuItem("Fake Region", !hasFakeEvent, UIFakesColorRGBA,
-				EditorFakeSegmentEvent.EventShortDescription, row, () =>
-				{
-					var fakeLength = currentRateAlteringEvent!.GetSecondsPerRow() * SMCommon.MaxValidDenominator;
-					return EditorEvent.CreateEvent(EventConfig.CreateFakeConfig(chart, row, fakeLength));
-				});
+				EditorFakeSegmentEvent.EventShortDescription, row,
+				() => EditorEvent.CreateEvent(EventConfig.CreateFakeConfig(chart, row)));
 			DrawAddEventMenuItem("Ticks", !hasTickCountEvent, UITicksColorRGBA,
 				EditorTickCountEvent.EventShortDescription, row,
 				() => EditorEvent.CreateEvent(EventConfig.CreateTickCountConfig(chart, row)));

StepManiaEditorTests/TestFakes.cs
@@ -39,9 +39,8 @@ public class TestFakes
 	{
 		var c = CreateTestChart();
 		const int row = SMCommon.RowsPerMeasure;
-		const double time = 1.0;
 		const int len = SMCommon.MaxValidDenominator;
-		c.AddEvent(EditorEvent.CreateEvent(EventConfig.CreateFakeConfig(c, row, time)));
+		c.AddEvent(EditorEvent.CreateEvent(EventConfig.CreateFakeConfig(c, row, len)));
 		Assert.AreEqual(1, c.GetFakes().GetCount());
 
 		var eventBeforeFake = EditorEvent.CreateEvent(EventConfig.CreateTapConfig(c, row - 1, 0));

StepManiaEditorTests/TestRateAlteringEventTree.cs
@@ -61,7 +61,7 @@ public class TestRateAlteringEventTree
 							config = EventConfig.CreateTickCountConfig(c, row);
 							break;
 						case nameof(FakeSegment):
-							config = EventConfig.CreateFakeConfig(c, row, 1.0);
+							config = EventConfig.CreateFakeConfig(c, row);
 							break;
 						case nameof(Multipliers):
 							config = EventConfig.CreateMultipliersConfig(c, row);

commit e874eb89e3ff4a2f4c9faec66f8cda20dfdb369d

Fix fake segments not rendering regions in constant time spacing. Regression from 05c2b5c7386240eafae2a4e877a42038dafe1d1b.

StepManiaEditor/EditorEvents/EditorFakeSegmentEvent.cs
@@ -181,6 +181,13 @@ internal sealed class EditorFakeSegmentEvent : EditorEvent, IEquatable<EditorFak
 		return GetRow() + FakeSegmentEvent.LengthIntegerPosition;
 	}
 
+	public override double GetEndChartTime()
+	{
+		var endChartTime = 0.0;
+		EditorChart.TryGetTimeFromChartPosition(GetEndChartPosition(), ref endChartTime);
+		return endChartTime;
+	}
+
 	public override void Draw(TextureAtlas textureAtlas, SpriteBatch spriteBatch, ArrowGraphicManager arrowGraphicManager)
 	{
 		if (Alpha <= 0.0f)