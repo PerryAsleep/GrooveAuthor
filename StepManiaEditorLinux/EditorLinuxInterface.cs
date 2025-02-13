bff20b887a4750fef4d5e1d93ec50482bce788dd
Perry Lynch <PerryKLynch@gmail.com>
2025-02-12 20:35:14 -0500

    Add file open/save dialogs for linux.

--- StepManiaEditorLinux/EditorLinuxInterface.cs
+++ StepManiaEditorLinux/EditorLinuxInterface.cs
@@ -3,6 +3,7 @@ using Fumen;
 using StepManiaEditor;
 using Gtk;
 using Microsoft.Xna.Framework;
+using System.Text;
 
 namespace StepManiaEditorLinux;
 
@@ -62,20 +63,66 @@ internal sealed class EditorLinuxInterface : IEditorPlatform
 
 	public (bool, string) ShowSaveSimFileDialog(string initialDirectory, FileFormatType? fileFormatType)
 	{
-		return (false, null);
+		var confirmed = false;
+		string fileName = null;
+		var dialog = new FileChooserDialog("Save As...",
+		null,
+		FileChooserAction.Save,
+		"Cancel", ResponseType.Cancel,
+		"Save", ResponseType.Accept);
+		dialog.Modal = true;
+		dialog.KeepAbove = true;
+
+		var sscFileFilter = new FileFilter();
+		sscFileFilter.Name = "SSC File";
+		sscFileFilter.AddPattern("*.ssc");
+		dialog.AddFilter(sscFileFilter);
+
+		var smFileFilter = new FileFilter();
+		smFileFilter.Name = "SM File";
+		smFileFilter.AddPattern("*.sm");
+		dialog.AddFilter(smFileFilter);
+
+		dialog.Filter = fileFormatType == FileFormatType.SM ? smFileFilter : sscFileFilter;
+
+		dialog.SetCurrentFolder(initialDirectory);
+
+		if (dialog.Run() == (int)ResponseType.Accept)
+		{
+			confirmed = true;
+			fileName = dialog.Filename;
+		}
+		dialog.Destroy();
+		return (confirmed, fileName);
 	}
 
 	public (bool, string) ShowOpenSimFileDialog(string initialDirectory)
 	{
 		var openedFile = false;
 		string fileName = null;
-		var dialog = new FileChooserDialog("Open simfile",
+		var dialog = new FileChooserDialog("Open File",
 		null,
 		FileChooserAction.Open,
 		"Cancel", ResponseType.Cancel,
 		"Open", ResponseType.Accept);
 		dialog.Modal = true;
 		dialog.KeepAbove = true;
+
+		var simFileFilter = new FileFilter();
+		simFileFilter.Name = "StepMania Files (*.sm,*.ssc)";
+		simFileFilter.AddPattern("*.sm");
+		simFileFilter.AddPattern("*.ssc");
+		dialog.AddFilter(simFileFilter);
+
+		var allFileFilter = new FileFilter();
+		allFileFilter.Name = "All Files (*.*)";
+		allFileFilter.AddPattern("*.*");
+		dialog.AddFilter(allFileFilter);
+
+		dialog.Filter = simFileFilter;
+
+		dialog.SetCurrentFolder(initialDirectory);
+
 		if (dialog.Run() == (int)ResponseType.Accept)
 		{
 			openedFile = true;
@@ -88,7 +135,63 @@ internal sealed class EditorLinuxInterface : IEditorPlatform
 	public string BrowseFile(string name, string initialDirectory, string currentFileRelativePath, List<string[]> extensionTypes,
 		bool includeAllFiles)
 	{
-		return null;
+		string relativePath = null;
+		var startInitialDirectory = initialDirectory;
+		if (!string.IsNullOrEmpty(currentFileRelativePath))
+		{
+			initialDirectory = Path.Combine(initialDirectory, currentFileRelativePath);
+			initialDirectory = System.IO.Path.GetDirectoryName(initialDirectory);
+		}
+
+		var dialog = new FileChooserDialog($"Open {name} File",
+		null,
+		FileChooserAction.Open,
+		"Cancel", ResponseType.Cancel,
+		"Open", ResponseType.Accept);
+		dialog.Modal = true;
+		dialog.KeepAbove = true;
+
+		var filter = new FileFilter();
+		var sb = new StringBuilder();
+		sb.Append(name);
+		sb.Append(" Files (");
+		var first = true;
+		foreach (var extensions in extensionTypes)
+		{
+			foreach (var extension in extensions)
+			{
+				if (!first)
+					sb.Append(',');
+				var pattern = $"*.{extension}";
+				sb.Append(pattern);
+				filter.AddPattern(pattern);
+				first = false;
+			}
+		}
+		sb.Append(')');
+		filter.Name = sb.ToString();
+		dialog.AddFilter(filter);
+
+		if (includeAllFiles)
+		{
+			var allFileFilter = new FileFilter();
+			allFileFilter.Name = "All Files (*.*)";
+			allFileFilter.AddPattern("*.*");
+			dialog.AddFilter(allFileFilter);
+		}
+
+		dialog.Filter = filter;
+
+		if (!string.IsNullOrEmpty(initialDirectory))
+			dialog.SetCurrentFolder(initialDirectory);
+
+		if (dialog.Run() == (int)ResponseType.Accept)
+		{
+			var fileName = dialog.Filename;
+			relativePath = Path.GetRelativePath(startInitialDirectory, fileName);
+		}
+		dialog.Destroy();
+		return relativePath;
 	}
 
 	#endregion File I/O
