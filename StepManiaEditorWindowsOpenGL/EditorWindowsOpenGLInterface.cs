02c29b0c643076bb74894bc4a4d4cb331c0bc539
Perry Lynch <perryklynch@gmail.com>
2025-02-17 14:57:14 -0500

    Add Windows OpenGL project mainly for ease of testing OpenGL since debugging on Linux is more challenging.

---
+++ StepManiaEditorWindowsOpenGL/EditorWindowsOpenGLInterface.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Fumen;
+using Microsoft.Xna.Framework;
+using StepManiaEditor;
+
+namespace StepManiaEditorWindowsOpenGL;
+
+/// <summary>
+/// Windows platform implementation of IEditorPlatform.
+/// </summary>
+internal sealed class EditorWindowsOpenGLInterface : IEditorPlatform
+{
+	private Editor Editor;
+
+	public void SetEditor(Editor editor)
+	{
+		Editor = editor;
+	}
+
+	public void InitializeWindowHandleCallbacks(bool maximized)
+	{
+	}
+
+	#region Drag and Drop
+
+	/// <summary>
+	/// Called when dragging a file into the window.
+	/// </summary>
+	public void DragEnter(object sender, DragEventArgs e)
+	{
+	}
+
+	/// <summary>
+	/// Called when dropping a file into the window.
+	/// </summary>
+	public void DragDrop(object sender, DragEventArgs e)
+	{
+	}
+
+	#endregion Drag and Drop
+
+	#region Window Size
+
+	public void SetResolution(int x, int y)
+	{
+	}
+
+	public bool IsMaximized()
+	{
+		return false;
+	}
+
+	#endregion Window Size
+
+	#region Sounds
+
+	public void PlayExclamationSound()
+	{
+		SystemSounds.Exclamation.Play();
+	}
+
+	#endregion Sounds
+
+	#region File I/O
+
+	public (bool, string) ShowSaveSimFileDialog(string initialDirectory, string fileName, FileFormatType? fileFormatType)
+	{
+		var saveFileDialog = new SaveFileDialog();
+		saveFileDialog.Filter = "SSC File|*.ssc|SM File|*.sm";
+		saveFileDialog.Title = "Save As...";
+		saveFileDialog.FilterIndex = 0;
+		if (fileFormatType == FileFormatType.SM)
+			saveFileDialog.FilterIndex = 2;
+		saveFileDialog.InitialDirectory = initialDirectory;
+		saveFileDialog.FileName = fileName;
+		var confirmed = saveFileDialog.ShowDialog() == DialogResult.OK;
+		return (confirmed, saveFileDialog.FileName);
+	}
+
+	public (bool, string) ShowOpenSimFileDialog(string initialDirectory)
+	{
+		using var openFileDialog = new OpenFileDialog();
+		openFileDialog.InitialDirectory = initialDirectory;
+		openFileDialog.Filter = "StepMania Files (*.sm,*.ssc)|*.sm;*.ssc|All files (*.*)|*.*";
+		openFileDialog.FilterIndex = 1;
+		var confirmed = openFileDialog.ShowDialog() == DialogResult.OK;
+		return (confirmed, openFileDialog.FileName);
+	}
+
+	private static string FileOpenFilter(string name, List<string[]> extensionTypes, bool includeAllFiles)
+	{
+		var sb = new StringBuilder();
+		sb.Append(name);
+		sb.Append(" Files (");
+		var first = true;
+		foreach (var extensions in extensionTypes)
+		{
+			foreach (var extension in extensions)
+			{
+				if (!first)
+					sb.Append(",");
+				sb.Append("*.");
+				sb.Append(extension);
+				first = false;
+			}
+		}
+
+		sb.Append(")|");
+		first = true;
+		foreach (var extensions in extensionTypes)
+		{
+			foreach (var extension in extensions)
+			{
+				if (!first)
+					sb.Append(";");
+				sb.Append("*.");
+				sb.Append(extension);
+				first = false;
+			}
+		}
+
+		if (includeAllFiles)
+		{
+			sb.Append("|All files (*.*)|*.*");
+		}
+
+		return sb.ToString();
+	}
+
+	public string BrowseFile(string name, string initialDirectory, string currentFileRelativePath, List<string[]> extensionTypes,
+		bool includeAllFiles)
+	{
+		var filter = FileOpenFilter(name, extensionTypes, includeAllFiles);
+
+		string relativePath = null;
+		using var openFileDialog = new OpenFileDialog();
+		var startInitialDirectory = initialDirectory;
+		if (!string.IsNullOrEmpty(currentFileRelativePath))
+		{
+			initialDirectory = Path.Combine(initialDirectory, currentFileRelativePath);
+			initialDirectory = System.IO.Path.GetDirectoryName(initialDirectory);
+		}
+
+		openFileDialog.InitialDirectory = initialDirectory ?? "";
+		openFileDialog.Filter = filter;
+		openFileDialog.FilterIndex = 1;
+		openFileDialog.Title = $"Open {name} File";
+
+		if (openFileDialog.ShowDialog() == DialogResult.OK)
+		{
+			var fileName = openFileDialog.FileName;
+			relativePath = Path.GetRelativePath(startInitialDirectory, fileName);
+		}
+
+		return relativePath;
+	}
+
+	#endregion File I/O
+
+	#region Clipboard
+
+	public void CopyToClipboard(string text)
+	{
+		Clipboard.SetText(text);
+	}
+
+	#endregion Clipboard
+
+	#region Application Focus
+
+	public bool IsApplicationFocused()
+	{
+		var activatedHandle = GetForegroundWindow();
+		if (activatedHandle == IntPtr.Zero)
+			return false;
+
+		GetWindowThreadProcessId(activatedHandle, out var activeProcId);
+		return activeProcId == Process.GetCurrentProcess().Id;
+	}
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+	private static extern IntPtr GetForegroundWindow();
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+	#endregion Application Focus
+
+	public void Update(GameTime gameTime)
+	{
+	}
+}
---
+++ StepManiaEditorWindowsOpenGL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StepManiaEditor;
+using StepManiaEditorWindowsOpenGL;
+
+public static class Program
+{
+	[STAThread]
+	private static void Main()
+	{
+		using var editor = new Editor(new EditorWindowsOpenGLInterface());
+		editor.Run();
+	}
+}
