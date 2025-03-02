5969ad63cd2ad2b4e02c7528d9f4af74aa497ba7
Perry Lynch <perryklynch@gmail.com>
2025-03-02 18:44:56 -0500

    Implement DPI scaling logic for Linux.

--- StepManiaEditorWindowsOpenGL/Program.cs
+++ StepManiaEditorWindowsOpenGL/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using StepManiaEditor;
 using StepManiaEditorWindowsOpenGL;
 
 public static class Program
 {
+	[DllImport("user32.dll")]
+	private static extern bool SetProcessDPIAware();
+
 	[STAThread]
 	private static void Main()
 	{
+		// At runtime set this process to be DPI aware. This is needed for
+		// us to ignore DPI scaling, which we want to do to avoid Windows
+		// performing bitmap scaling on the window and making it blurry. The
+		// application will handle scaling fonts and UI values based on the
+		// monitor's DPI scaling value.
+		SetProcessDPIAware();
+
 		using var editor = new Editor(new EditorWindowsOpenGLInterface());
 		editor.Run();
 	}
