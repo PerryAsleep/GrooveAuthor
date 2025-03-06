6276385e3940e1c3b45790d84581c346dd56ab3f
Perry Lynch <PerryKLynch@gmail.com>
2025-03-05 21:04:48 -0500

    Add linux install script.

---
+++ StepManiaEditorLinux/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -eu
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run as root."
+    exit 1
+fi
+
+# Back up the existing install.
+if [ -d /opt/GrooveAuthor ]; then
+    echo "Backing up current GrooveAuthor installation."
+    mv /opt/GrooveAuthor /opt/GrooveAuthor.old
+fi
+
+cd "$(dirname "$0")"
+
+# Enure the /opt directory exists.
+if ! [ -d /opt ]; then
+    echo "Creating /opt directory."
+    install -d -m 755 -o root -g root /opt
+fi
+
+# Copy the application.
+echo "Installing GrooveAuthor."
+cp -R --preserve=mode,timestamps GrooveAuthor /opt
+
+# Configure the dekstop entry.
+echo "Adding desktop entry."
+ln -sf /opt/GrooveAuthor/GrooveAuthor.desktop /usr/share/applications
+
+# Remove backup.
+if [ -d /opt/GrooveAuthor.old ]; then
+    echo "Removing previous GrooveAuthor backup."
+    rm -rf /opt/GrooveAuthor.old
+fi
+
+echo "Done."
