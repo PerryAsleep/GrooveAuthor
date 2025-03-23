6654ff705d595ba6806678fb67eac71fd6c2c168
Perry Lynch <perryklynch@gmail.com>
2025-03-22 20:08:43 -0400

    #42 Address issues where tempos would be incorrectly interpreted as Specified instead of Actual. Improve default Specified tempos when switching from other modes to Specified.

---
+++ StepManiaEditor/EditorActions/ActionSetDisplayTempoMode.cs
@@ -0,0 +1,60 @@
+﻿using Fumen;
+
+namespace StepManiaEditor;
+
+/// <summary>
+/// EditorAction for an EditorChart's DisplayTempo's DisplayTempoMode.
+/// When changing mode we may want to alter other properties of the DisplayTempo, like it's
+/// specified values.
+/// </summary>
+internal sealed class ActionSetDisplayTempoMode : EditorAction
+{
+	private readonly EditorChart Chart;
+	private readonly DisplayTempoMode Mode;
+	private readonly DisplayTempoMode PreviousMode;
+	private readonly double PreviousSpecifiedMin;
+	private readonly double PreviousSpecifiedMax;
+	private readonly bool PreviousShouldAllowEditsOfMax;
+
+	public ActionSetDisplayTempoMode(EditorChart chart, DisplayTempoMode mode) : base(false, false)
+	{
+		Chart = chart;
+		Mode = mode;
+		PreviousMode = Chart.DisplayTempoMode;
+		PreviousSpecifiedMin = Chart.DisplayTempoSpecifiedTempoMin;
+		PreviousSpecifiedMax = Chart.DisplayTempoSpecifiedTempoMax;
+		PreviousShouldAllowEditsOfMax = Chart.DisplayTempoShouldAllowEditsOfMax;
+	}
+
+	public override bool AffectsFile()
+	{
+		return true;
+	}
+
+	public override string ToString()
+	{
+		return $"Set display tempo mode to {Mode}.";
+	}
+
+	protected override void DoImplementation()
+	{
+		Chart.DisplayTempoMode = Mode;
+
+		// When changing to a specified mode, use the current actual min and max tempo.
+		if (Mode == DisplayTempoMode.Specified && PreviousMode != DisplayTempoMode.Specified)
+		{
+			Chart.DisplayTempoSpecifiedTempoMin = Chart.GetMinTempo();
+			Chart.DisplayTempoSpecifiedTempoMax = Chart.GetMaxTempo();
+			if (!Chart.DisplayTempoSpecifiedTempoMin.DoubleEquals(Chart.DisplayTempoSpecifiedTempoMax))
+				Chart.DisplayTempoShouldAllowEditsOfMax = true;
+		}
+	}
+
+	protected override void UndoImplementation()
+	{
+		Chart.DisplayTempoMode = PreviousMode;
+		Chart.DisplayTempoSpecifiedTempoMin = PreviousSpecifiedMin;
+		Chart.DisplayTempoSpecifiedTempoMax = PreviousSpecifiedMax;
+		Chart.DisplayTempoShouldAllowEditsOfMax = PreviousShouldAllowEditsOfMax;
+	}
+}
