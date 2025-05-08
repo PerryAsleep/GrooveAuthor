PerryAsleep/GrooveAuthor

commit bf19b4103870dc3cf007e8d972fee3442864ab47

Fix some Observers maintaining references to objects expected to be garbage collected.

StepManiaEditor/EditorChart.cs
@@ -490,6 +490,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 	{
 		Id = Guid.NewGuid();
 		WorkQueue = new WorkQueue();
+		WorkQueue.AddObserver(this);
 		ExpressedChartConfigInternal = ExpressedChartConfigManager.DefaultExpressedChartDynamicConfigGuid;
 
 		OriginalChartExtras = chart.Extras;
@@ -551,6 +552,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 	{
 		Id = Guid.NewGuid();
 		WorkQueue = new WorkQueue();
+		WorkQueue.AddObserver(this);
 
 		ExpressedChartConfigInternal = ExpressedChartConfigManager.DefaultExpressedChartDynamicConfigGuid;
 
@@ -603,6 +605,7 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 	{
 		Id = Guid.NewGuid();
 		WorkQueue = new WorkQueue();
+		WorkQueue.AddObserver(this);
 
 		ExpressedChartConfigInternal = other.ExpressedChartConfigInternal;
 
@@ -913,6 +916,16 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 	}
 
 	#endregion Constructors
+
+	#region Clean-up
+
+	public void RemoveObservers()
+	{
+		WorkQueue.RemoveObserver(this);
+		StepTotals?.RemoveObservers();
+	}
+
+	#endregion Clean-up
 
 	#region Idenfitication
 

StepManiaEditor/Editor.cs
@@ -5389,6 +5389,7 @@ public sealed class Editor :
 			GarbageCollectFrame = 2;
 
 		ActiveSong?.RemoveObservers(this, this);
+		ActiveSong?.RemoveObservers();
 
 		// Close any UI which holds on to Song/Chart state.
 		UIAutogenChart.Instance.Close();

StepManiaEditor/EditorSong.cs
@@ -785,6 +785,22 @@ internal sealed class EditorSong : Notifier<EditorSong>, Fumen.IObserver<WorkQue
 
 	#endregion Constructors
 
+	#region Clean-up
+
+	public void RemoveObservers()
+	{
+		WorkQueue.RemoveObserver(this);
+		foreach (var editorChartsForChartType in Charts)
+		{
+			foreach (var editorChart in editorChartsForChartType.Value)
+			{
+				editorChart.RemoveObservers();
+			}
+		}
+	}
+
+	#endregion Clean-up
+
 	#region EditorChart
 
 	public IReadOnlyList<EditorChart> GetCharts(ChartType chartType)

StepManiaEditor/StepDensity.cs
@@ -140,6 +140,12 @@ internal sealed class StepDensity : Notifier<StepDensity>, Fumen.IObserver<Prefe
 		Notify(NotificationMeasuresChanged, this);
 	}
 
+	public void RemoveObservers()
+	{
+		Preferences.Instance.PreferencesStream.RemoveObserver(this);
+		EditorChart.RemoveObserver(this);
+	}
+
 	/// <summary>
 	/// Returns the array of all Measures.
 	/// </summary>

StepManiaEditor/StepTotals.cs
@@ -122,6 +122,11 @@ internal sealed class StepTotals : IReadOnlyStepTotals
 		Clear();
 	}
 
+	public void RemoveObservers()
+	{
+		StepDensity?.RemoveObservers();
+	}
+
 	public void InitializeStepDensity()
 	{
 		StepDensity = new StepDensity(EditorChart);

commit f10edc61656c1f780cb2865788dcaec852d8e80a

Fix chart saving running work queue notifications off the main thread. Regression from bf19b4103870dc3cf007e8d972fee3442864ab47. In practice this wouldn't cause any problems as this specific notification was not being used for any thread-sensitive logic, but it was causing a debug assert.

StepManiaEditor/EditorChart.cs
@@ -3538,48 +3538,50 @@ internal sealed class EditorChart : Notifier<EditorChart>, Fumen.IObserver<WorkQ
 		var customProperties = saveParameters.OmitCustomSaveData ? null : new Dictionary<string, string>();
 
 		// Enqueue a task to save this EditorChart to a Chart.
-		WorkQueue.Enqueue(new Task(() =>
-			{
-				chart.Extras = new Extras(OriginalChartExtras);
-
-				chart.Type = ChartTypeString(ChartType);
-				chart.DifficultyType = ChartDifficultyType.ToString();
-				chart.NumInputs = NumInputs;
-				chart.NumPlayers = MaxPlayers;
-				chart.DifficultyRating = Rating;
-				chart.Extras.AddDestExtra(TagMusic, MusicPath, true);
-				chart.Tempo = DisplayTempo.ToString();
-
-				if (saveParameters.AnonymizeSaveData)
+		// Run this on the main thread so the WorkQueue notifications are processed on the main thread.
+		MainThreadDispatcher.RunOnMainThread(() =>
+			WorkQueue.Enqueue(new Task(() =>
 				{
-					chart.Description = null;
-					chart.Author = null;
-					chart.Extras.RemoveSourceExtra(TagChartName);
-					chart.Extras.RemoveSourceExtra(TagChartStyle);
-				}
-				else
-				{
-					chart.Description = Description;
-					chart.Author = Credit;
-					chart.Extras.AddDestExtra(TagChartName, Name, true);
-					chart.Extras.AddDestExtra(TagChartStyle, Style, true);
-				}
+					chart.Extras = new Extras(OriginalChartExtras);
 
-				// Always set the chart's music offset. Clear any existing extra tag that may be stale.
-				chart.ChartOffsetFromMusic = GetMusicOffset();
-				chart.Extras.RemoveSourceExtra(TagOffset);
+					chart.Type = ChartTypeString(ChartType);
+					chart.DifficultyType = ChartDifficultyType.ToString();
+					chart.NumInputs = NumInputs;
+					chart.NumPlayers = MaxPlayers;
+					chart.DifficultyRating = Rating;
+					chart.Extras.AddDestExtra(TagMusic, MusicPath, true);
+					chart.Tempo = DisplayTempo.ToString();
 
-				if (!saveParameters.OmitCustomSaveData)
-					SerializeCustomChartData(customProperties);
+					if (saveParameters.AnonymizeSaveData)
+					{
+						chart.Description = null;
+						chart.Author = null;
+						chart.Extras.RemoveSourceExtra(TagChartName);
+						chart.Extras.RemoveSourceExtra(TagChartStyle);
+					}
+					else
+					{
+						chart.Description = Description;
+						chart.Author = Credit;
+						chart.Extras.AddDestExtra(TagChartName, Name, true);
+						chart.Extras.AddDestExtra(TagChartStyle, Style, true);
+					}
 
-				var layer = new Layer
-				{
-					Events = GenerateSmEvents(),
-				};
-				chart.Layers.Add(layer);
-			}),
-			// When complete, call the given callback with the saved data.
-			() => callback(chart, customProperties));
+					// Always set the chart's music offset. Clear any existing extra tag that may be stale.
+					chart.ChartOffsetFromMusic = GetMusicOffset();
+					chart.Extras.RemoveSourceExtra(TagOffset);
+
+					if (!saveParameters.OmitCustomSaveData)
+						SerializeCustomChartData(customProperties);
+
+					var layer = new Layer
+					{
+						Events = GenerateSmEvents(),
+					};
+					chart.Layers.Add(layer);
+				}),
+				// When complete, call the given callback with the saved data.
+				() => callback(chart, customProperties)));
 	}
 
 	#endregion Saving