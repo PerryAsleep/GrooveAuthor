7516e75fb7936be44b01e03622efd31f1d39a1dc
LeBlusz <pyrowski5@wp.pl>
2026-08-02 15:22:05 +0200

    Forcing locale in linux app

--- StepManiaEditorLinux/Program.cs
+++ StepManiaEditorLinux/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using StepManiaEditor;
 using StepManiaEditorLinux;
 
@@ -6,9 +7,13 @@ using StepManiaEditorLinux;
 
 public static class Program
 {
+	[DllImport("libc", CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr setlocale(int category, string locale);
+
 	[STAThread]
 	private static void Main(string[] args)
 	{
+		setlocale(1, "C");
 		using var editor = new Editor(args, new EditorLinuxInterface());
 		editor.Run();
 	}
